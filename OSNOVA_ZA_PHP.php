2c3ad998ad1c062da05c2a44d308e701c4ff2754
GhostGapy <gmelansek@gmail.com>
2022-02-21 09:57:26 +0100

    push

--- OSNOVA_ZA_PHP.php
+++ OSNOVA_ZA_PHP.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang='sl'>
+<html lang="sl">
 <head>
     <meta charset="UTF-8">
-    <title>Vaja</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
     <?php
-        
+
     ?>
 </body>
 </html>
